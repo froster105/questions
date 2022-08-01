8d48ce95f191adbdc8aff7910287f511dffcd3d5
Naman Singh <97724270+froster105@users.noreply.github.com>
2022-08-01 14:18:33 +0530

    Create problem2.java

---
+++ problem2.java
@@ -0,0 +1,6 @@
+ public static void generator(int input) {
+        System.out.print(1+",");
+        for (int i = 1; i < input; i++) {
+            System.out.print(2 * i + 1 + ",");
+        }
+    }
